856408bd4534b7d31049dbe33ee7a0851b0d62d7
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-06-05 00:25:06 +0300

    add compute-engine file

---
+++ terraform/dev/compute-engine.tf
@@ -0,0 +1,39 @@
+resource "google_compute_instance" "adesanu" {
+  name         = var.instance_name
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    "ssk-keys" = var.pub_key
+  }
+
+}
+
+# data "google_client_openid_userinfo" "me" {
+# }
+
+# resource "google_os_login_ssh_public_key" "default" {
+#   user = data.google_client_openid_userinfo.me.email
+#   key  = file("~/.ssh/gcp-instance-adesanu.pub") # path/to/ssl/id_rsa.pub
+# }
